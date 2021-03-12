12409c915bae91cb2d1921bfcdc945def2f25d30
lxr1907 <418982099@qq.com>
2021-03-13 00:44:17 +0800

    1

--- src/main/java/easyJava/job/HuobiDataSync.java
+++ src/main/java/easyJava/job/HuobiDataSync.java
@@ -43,17 +43,22 @@ public class HuobiDataSync {
 			@Override
 			public void run() {
 				while (true) {
-					for (String symbol : symbolSet) {
-						Map<String, String> headers = new HashMap<String, String>();
-						Map<String, String> params = new HashMap<String, String>();
-						params.put("symbol", symbol);
-						params.put("period", "1day");
-						params.put("size", "1");
-						String result = HttpsUtils.Get(HUOBI_API_URL_PRE + MARKET_KLINE, headers, params);
-						if (result != null) {
-							HuobiKlineEntity entity = JSON.parseObject(result, HuobiKlineEntity.class);
-							redisTemplate.opsForHash().put(MARKET_KLINE, symbol, entity);
+					try {
+						for (String symbol : symbolSet) {
+							Map<String, String> headers = new HashMap<String, String>();
+							Map<String, String> params = new HashMap<String, String>();
+							params.put("symbol", symbol);
+							params.put("period", "1day");
+							params.put("size", "1");
+							String result = HttpsUtils.Get(HUOBI_API_URL_PRE + MARKET_KLINE, headers, params);
+							System.out.println(result);
+							if (result != null) {
+								HuobiKlineEntity entity = JSON.parseObject(result, HuobiKlineEntity.class);
+								redisTemplate.opsForHash().put(MARKET_KLINE, symbol, entity);
+							}
 						}
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
 					}
 					try {
 						Thread.sleep(10 * 1000);
@@ -67,6 +72,7 @@ public class HuobiDataSync {
 	}
 
 	public static void main(String[] args) {
+		/// https://api-aws.huobi.pro/market/history/kline?symbol=btcusdt&period=1day&size=1
 		Map<String, String> headers = new HashMap<String, String>();
 		Map<String, String> params = new HashMap<String, String>();
 		params.put("symbol", "btcusdt");
