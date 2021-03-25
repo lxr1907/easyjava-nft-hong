b6eeddf3a6d2388acc1a9ce874d8a3eaa95cf5fc
lxr1907 <418982099@qq.com>
2021-03-25 23:54:25 +0800

    1

--- src/main/java/easyJava/controller/BtcpayController.java
+++ src/main/java/easyJava/controller/BtcpayController.java
@@ -3,8 +3,8 @@ package easyJava.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import javax.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +13,6 @@ import easyJava.utils.HttpsUtils;
 
 @RestController
 public class BtcpayController {
-	@Autowired
-	private RedisTemplate<String, Object> redisTemplate;
 
 	public static final String BTCPAY_URL="https://btcpay.lxrtalk.com";
 	public static final String GET_STORE="/api/v1/stores";
@@ -26,6 +24,13 @@ public class BtcpayController {
 		Map<String, String> headers=new HashMap<String, String>();
 		headers.put(TOKEN, TOKEN_VALUE);
 		var ret = HttpsUtils.Get(BTCPAY_URL+GET_STORE, headers);
+		return new ResponseEntity(ret);
+	}
+	@RequestMapping("/btcpay/api/v1/stores/{storeId}/invoices")
+	public ResponseEntity<?> createInvoice(@PathParam(value = "storeId") String  storeId) {
+		Map<String, String> headers=new HashMap<String, String>();
+		headers.put(TOKEN, TOKEN_VALUE);
+		var ret = HttpsUtils.Get(BTCPAY_URL+"/api/v1/stores/"+storeId+"/invoices", headers);
 		return new ResponseEntity(ret);
 	}
 
