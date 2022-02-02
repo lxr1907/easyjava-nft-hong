5a1379d27d174d80a0d46492ceacc6227f7b6747
lxr <418982099@qq.com>
2022-02-02 18:17:19 +0800

    生成随机数

--- src/main/java/easyJava/controller/NFTinviteCodeController.java
+++ src/main/java/easyJava/controller/NFTinviteCodeController.java
@@ -3,7 +3,6 @@ package easyJava.controller;
 import easyJava.dao.master.BaseDao;
 import easyJava.entity.BaseModel;
 import easyJava.entity.ResponseEntity;
-import easyJava.utils.GenerateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +14,16 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-public class NFTController {
+public class NFTinviteCodeController {
 	@Autowired
 	BaseDao baseDao;
 
 	@Autowired
 	private RedisTemplate<String, Object> redisTemplate;
 
-	public static final String AddressInvite_MANAGE = "address_invite";
+	public static final String InviteCode_MANAGE = "invite_code";
 
-	@RequestMapping("/getAddressInviteList")
+	@RequestMapping("/getInviteCodeList")
 	public ResponseEntity<?> login(@RequestParam Map<String, Object> map) {
 		if (map.get("pageSize") == null || map.get("pageSize").toString().length() == 0) {
 			return new ResponseEntity(400, "pageSize不能为空！");
@@ -32,7 +31,7 @@ public class NFTController {
 		if (map.get("pageNo") == null || map.get("pageNo").toString().length() == 0) {
 			return new ResponseEntity(400, "pageNo不能为空！");
 		}
-		map.put("tableName", AddressInvite_MANAGE);
+		map.put("tableName", InviteCode_MANAGE);
 		BaseModel baseModel = new BaseModel();
 		baseModel.setPageSize(Integer.parseInt(map.get("pageSize").toString()));
 		baseModel.setPageNo(Integer.parseInt(map.get("pageNo").toString()));
@@ -43,33 +42,32 @@ public class NFTController {
 		return new ResponseEntity(retmap, count, baseModel);
 	}
 
-	@RequestMapping("/insertAddressInvite")
-	public ResponseEntity insertAddressInvite(@RequestParam Map<String, Object> map) {
-		if (map.get("name") == null || map.get("name").toString().length() == 0) {
+	@RequestMapping("/insertInviteCode")
+	public ResponseEntity insertInviteCode(@RequestParam Map<String, Object> map) {
+		if (map.get("address") == null || map.get("address").toString().length() == 0) {
 			return new ResponseEntity(400, "address不能为空！");
 		}
-		map.put("tableName", AddressInvite_MANAGE);
-		map.put("invite_code",Long.parseLong(GenerateUtils.getRandomNickname(10)));
+		map.put("tableName", InviteCode_MANAGE);
 		int count = baseDao.insertIgnoreBase(map);
 		return new ResponseEntity(count);
 	}
 
-	@RequestMapping("/updateAddressInvite")
+	@RequestMapping("/updateInviteCode")
 	public ResponseEntity updateBaseByPrimaryKey(@RequestParam Map<String, Object> map) {
 		if (map.get("id") == null || map.get("id").toString().trim().length() == 0) {
 			return new ResponseEntity(400, "id不能为空！");
 		}
-		map.put("tableName", AddressInvite_MANAGE);
+		map.put("tableName", InviteCode_MANAGE);
 		int count = baseDao.updateBaseByPrimaryKey(map);
 		return new ResponseEntity(count);
 	}
 
-	@RequestMapping("/getAddressInvite")
-	public ResponseEntity getAddressInvite(@RequestParam Map<String, Object> map) {
-		if (map.get("name") == null || map.get("name").toString().length() == 0) {
+	@RequestMapping("/getInviteCode")
+	public ResponseEntity getInviteCode(@RequestParam Map<String, Object> map) {
+		if (map.get("address") == null || map.get("address").toString().length() == 0) {
 			return new ResponseEntity(400, "address不能为空！");
 		}
-		map.put("tableName", AddressInvite_MANAGE);
+		map.put("tableName", InviteCode_MANAGE);
 		BaseModel baseModel = new BaseModel();
 		baseModel.setPageSize(1);
 		baseModel.setPageNo(1);
--- src/main/java/easyJava/utils/GenerateUtils.java
+++ src/main/java/easyJava/utils/GenerateUtils.java
@@ -1,6 +1,7 @@
 package easyJava.utils;
 
 import java.util.Date;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -18,5 +19,24 @@ public class GenerateUtils {
 		long increment = entityIdCounter.getAndIncrement();
 		id = increment + "" + new Date().getTime();
 		return id;
+	}
+
+	/**
+	 * java生成随机数字10位数
+	 *
+	 * @param
+	 * @return
+	 */
+	public static String getRandomNickname(int length) {
+		String val = "";
+		Random random = new Random();
+		for (int i = 0; i < length; i++) {
+			val += String.valueOf(random.nextInt(10));
+		}
+		return val;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("java生成随机数字10位数：" + getRandomNickname(10));
 	}
 }
