0dbedeeb314fa4a07bcba208b8f6965e42215da6
lxr <418982099@qq.com>
2022-02-06 23:11:31 +0800

    nft data import

--- src/main/java/easyJava/controller/NFTdataController.java
+++ src/main/java/easyJava/controller/NFTdataController.java
@@ -15,65 +15,70 @@ import java.util.Map;
 
 @RestController
 public class NFTdataController {
-	@Autowired
-	BaseDao baseDao;
+    @Autowired
+    BaseDao baseDao;
 
-	@Autowired
-	private RedisTemplate<String, Object> redisTemplate;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
-	public static final String NFTdata_MANAGE = "nft_data";
+    public static final String NFTdata_MANAGE = "nft_data";
+    public static final String baseUrl = "http://175.178.10.214:8090/easyJava/baseUrl/";
 
-	@RequestMapping("/getNFTdataList")
-	public ResponseEntity<?> login(@RequestParam Map<String, Object> map) {
-		if (map.get("pageSize") == null || map.get("pageSize").toString().length() == 0) {
-			return new ResponseEntity(400, "pageSize不能为空！");
-		}
-		if (map.get("pageNo") == null || map.get("pageNo").toString().length() == 0) {
-			return new ResponseEntity(400, "pageNo不能为空！");
-		}
-		map.put("tableName", NFTdata_MANAGE);
-		BaseModel baseModel = new BaseModel();
-		baseModel.setPageSize(Integer.parseInt(map.get("pageSize").toString()));
-		baseModel.setPageNo(Integer.parseInt(map.get("pageNo").toString()));
-		var retmap = new HashMap();
-		var list = baseDao.selectBaseList(map, baseModel);
-		int count = baseDao.selectBaseCount(map);
-		retmap.put("list", list);
-		return new ResponseEntity(retmap, count, baseModel);
-	}
+    @RequestMapping("/getNFTdataList")
+    public ResponseEntity<?> login(@RequestParam Map<String, Object> map) {
+        if (map.get("pageSize") == null || map.get("pageSize").toString().length() == 0) {
+            return new ResponseEntity(400, "pageSize不能为空！");
+        }
+        if (map.get("pageNo") == null || map.get("pageNo").toString().length() == 0) {
+            return new ResponseEntity(400, "pageNo不能为空！");
+        }
+        map.put("tableName", NFTdata_MANAGE);
+        BaseModel baseModel = new BaseModel();
+        baseModel.setPageSize(Integer.parseInt(map.get("pageSize").toString()));
+        baseModel.setPageNo(Integer.parseInt(map.get("pageNo").toString()));
+        var retmap = new HashMap();
+        var list = baseDao.selectBaseList(map, baseModel);
+        int count = baseDao.selectBaseCount(map);
+        retmap.put("list", list);
+        return new ResponseEntity(retmap, count, baseModel);
+    }
 
-	@RequestMapping("/insertNFTdata")
-	public ResponseEntity insertNFTdata(@RequestParam Map<String, Object> map) {
-		if (map.get("address") == null || map.get("address").toString().length() == 0) {
-			return new ResponseEntity(400, "address不能为空！");
-		}
-		map.put("tableName", NFTdata_MANAGE);
-		int count = baseDao.insertIgnoreBase(map);
-		return new ResponseEntity(count);
-	}
+    @RequestMapping("/insertNFTdata")
+    public ResponseEntity insertNFTdata(@RequestParam Map<String, Object> map) {
+        if (map.get("address") == null || map.get("address").toString().length() == 0) {
+            return new ResponseEntity(400, "address不能为空！");
+        }
+        map.put("tableName", NFTdata_MANAGE);
+        int count = baseDao.insertIgnoreBase(map);
+        return new ResponseEntity(count);
+    }
 
-	@RequestMapping("/updateNFTdata")
-	public ResponseEntity updateBaseByPrimaryKey(@RequestParam Map<String, Object> map) {
-		if (map.get("id") == null || map.get("id").toString().trim().length() == 0) {
-			return new ResponseEntity(400, "id不能为空！");
-		}
-		map.put("tableName", NFTdata_MANAGE);
-		int count = baseDao.updateBaseByPrimaryKey(map);
-		return new ResponseEntity(count);
-	}
+    @RequestMapping("/updateNFTdata")
+    public ResponseEntity updateBaseByPrimaryKey(@RequestParam Map<String, Object> map) {
+        if (map.get("id") == null || map.get("id").toString().trim().length() == 0) {
+            return new ResponseEntity(400, "id不能为空！");
+        }
+        map.put("tableName", NFTdata_MANAGE);
+        int count = baseDao.updateBaseByPrimaryKey(map);
+        return new ResponseEntity(count);
+    }
 
-	@RequestMapping("/getNFTdata")
-	public ResponseEntity getNFTdata(@RequestParam Map<String, Object> map) {
-		if (map.get("id") == null || map.get("id").toString().length() == 0) {
-			return new ResponseEntity(400, "id不能为空！");
-		}
-		map.put("tableName", NFTdata_MANAGE);
-		BaseModel baseModel = new BaseModel();
-		baseModel.setPageSize(1);
-		baseModel.setPageNo(1);
-		HashMap retmap = new HashMap();
-		List list = baseDao.selectBaseList(map, baseModel);
-		retmap.put("list", list);
-		return new ResponseEntity(retmap, 1, baseModel);
-	}
+    @RequestMapping("/getNFTdata")
+    public ResponseEntity getNFTdata(@RequestParam Map<String, Object> map) {
+        if (map.get("id") == null || map.get("id").toString().length() == 0) {
+            return new ResponseEntity(400, "id不能为空！");
+        }
+        map.put("tableName", NFTdata_MANAGE);
+        BaseModel baseModel = new BaseModel();
+        baseModel.setPageSize(1);
+        baseModel.setPageNo(1);
+        HashMap retmap = new HashMap();
+        List<Map> list = baseDao.selectBaseList(map, baseModel);
+        list.forEach(retMap -> {
+            String id = retMap.get("id").toString();
+            retMap.put("tokenUrl", baseUrl + id);
+        });
+        retmap.put("list", list);
+        return new ResponseEntity(retmap, 1, baseModel);
+    }
 }
