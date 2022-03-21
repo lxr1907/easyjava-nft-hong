8a7f3ae763285924999934ad618149be6d10bab1
418982099@qq.com <418982099@qq.com>
2022-03-22 00:12:51 +0800

    连接klay

--- src/main/java/easyJava/Configs.java
+++ src/main/java/easyJava/Configs.java
@@ -10,8 +10,7 @@ public class Configs {
     public static String getBtcpayHost() {
         return btcpayHost;
     }
-//
-//    @Value("${btcpay.host}")
+    @Value("${btcpay.host}")
     public void setBtcpayHost(String btcpayHost) {
         this.btcpayHost = btcpayHost;
     }
