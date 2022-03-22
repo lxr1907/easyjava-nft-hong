7aaae65b909f3dcf5d3e0ace2fa5e8954fe27f91
lxr <418982099@qq.com>
2022-03-22 18:34:19 +0800

    allowedOriginPatterns

--- src/main/java/easyJava/WebMvcConfig.java
+++ src/main/java/easyJava/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .maxAge(3600)
                 .allowCredentials(true);
