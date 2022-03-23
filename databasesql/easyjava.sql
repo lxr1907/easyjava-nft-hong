691d67cb45ead1d676103b909d6f35e19e6c8844
lxr <418982099@qq.com>
2022-03-23 16:15:51 +0800

    用户表设计

--- databasesql/easyjava.sql
+++ databasesql/easyjava.sql
@@ -15,14 +15,17 @@ Date: 2019-12-01 22:44:28
 
 SET FOREIGN_KEY_CHECKS=0;
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
+-- nft.`user` definition
+
 CREATE TABLE `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `account` varchar(255) NOT NULL COMMENT '账号',
   `password` varchar(255) NOT NULL COMMENT '密码',
+  `my_invite_code` varchar(255) NOT NULL COMMENT '我的邀请码',
+  `invite_code` varchar(255) NOT NULL COMMENT '邀请码',
+  `chr_address` varchar(255) NOT NULL COMMENT '合约币地址',
   `status` int(11) NOT NULL DEFAULT '1' COMMENT '状态1正常',
+  `chr_amount` bigint(20) NOT NULL DEFAULT '0' COMMENT '合约币数量',
+  `game_coin_amount` bigint(20) NOT NULL DEFAULT '0' COMMENT '游戏币数量',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
