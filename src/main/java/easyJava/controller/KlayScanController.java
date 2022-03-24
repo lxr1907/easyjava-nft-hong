ce6bc42873ca276914a3b26f091233a99a6292d8
lxr <418982099@qq.com>
2022-03-24 23:56:58 +0800

    定时扫描插入地址的交易列表txs

--- src/main/java/easyJava/controller/KlayScanController.java
+++ src/main/java/easyJava/controller/KlayScanController.java
@@ -1,23 +1,25 @@
 package easyJava.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.klaytn.caver.Caver;
-import com.klaytn.caver.account.Account;
 import com.klaytn.caver.methods.response.Bytes32;
 import com.klaytn.caver.methods.response.TransactionReceipt;
 import com.klaytn.caver.transaction.TxPropertyBuilder;
 import com.klaytn.caver.transaction.response.PollingTransactionReceiptProcessor;
 import com.klaytn.caver.transaction.response.TransactionReceiptProcessor;
 import com.klaytn.caver.transaction.type.ValueTransfer;
-import com.klaytn.caver.utils.Utils;
 import com.klaytn.caver.wallet.keyring.KeyringFactory;
 import com.klaytn.caver.wallet.keyring.SingleKeyring;
 import easyJava.dao.master.BaseDao;
-import easyJava.entity.BaseModel;
+import easyJava.entity.KlayTxsResult;
 import easyJava.entity.ResponseEntity;
+import easyJava.etherScan.EventList;
+import easyJava.utils.HttpUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,35 +27,35 @@ import org.web3j.crypto.CipherException;
 import org.web3j.protocol.exceptions.TransactionException;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
-public class KlayController {
-    private static final Logger logger = LoggerFactory.getLogger(KlayController.class);
+public class KlayScanController {
+    private static final Logger logger = LoggerFactory.getLogger(KlayScanController.class);
     @Autowired
     BaseDao baseDao;
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
-    public static final String Klay_MANAGE = "address_invite";
-    public static final String SYSTEM_PRIVATE = "6b9332b28c2a689f464994cb7be26485aba9a3471077cf8607eefe8f849c10f8";
-    public static final String SYSTEM_ADDRESS = "0xe61c910ac9A6629E88675Ba34E36620cFA966824";
-    public static final String Klay_HOST = "https://api.baobab.klaytn.net:8651/";
-    public static final String MY_KLAY_HOST = "http://43.132.248.207:8551";
+    public static final String KLAY_TXS_TABLE = "klay_txs";
+    public static final String KLAY_API_PRE = "https://api-baobab-v2.scope.klaytn.com/v2/accounts/";
+    public static final String TXS_API = "/txs";
 
-    /**
-     * 生成privateKey
-     *
-     * @return
-     */
-    public static String generatePrivate() {
-        String newPrivateKey = KeyringFactory.generateSingleKey();
-        return newPrivateKey;
+    @Scheduled(cron = "*/15 * * * * ?")
+    @RequestMapping("/scanKlayTxs")
+    public ResponseEntity<?> scanKlayTxs() {
+        //这个方法要在代码里写个定时器， 每隔 5或10秒 扫一次
+        KlayTxsResult result = getAddressTxs(KlayController.SYSTEM_ADDRESS);
+
+        List<Map<String, Object>> retList = result.getResult();
+        retList.forEach(map -> {
+            map.put("tableName", KLAY_TXS_TABLE);
+            baseDao.insertIgnoreBase(map);
+        });
+        return new ResponseEntity();
     }
 
     /**
@@ -61,22 +63,22 @@ public class KlayController {
      *
      * @return
      */
-    public static String getWalletAddress(String newPrivateKey) {
-        SingleKeyring newKeyring = KeyringFactory.createFromPrivateKey(newPrivateKey);
-        Account account = newKeyring.toAccount();
-        return account.getAddress();
+    public static KlayTxsResult getAddressTxs(String address) {
+        String result = HttpUtil.get(KLAY_API_PRE + address + TXS_API);
+        KlayTxsResult response = JSON.parseObject(result, KlayTxsResult.class);
+        return response;
     }
 
-    //  BigInteger value = new BigInteger(Utils.convertToPeb(BigDecimal.ONE, "KLAY"));
     public static void main(String[] args) {
         try {
-            sendingKLAY(SYSTEM_PRIVATE,"0x38bd8d9f0acda0ce533f44adcfd02b403f411de7",BigInteger.valueOf(1));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (TransactionException e) {
+            KlayTxsResult result = getAddressTxs(KlayController.SYSTEM_ADDRESS);
+            System.out.println(JSON.toJSONString(result));
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+
     /**
      * 发送klay
      *
@@ -88,7 +90,7 @@ public class KlayController {
      * @throws TransactionException
      */
     public static TransactionReceipt.TransactionReceiptData sendingKLAY(String fromPrivateKey, String toAddress, BigInteger value) throws IOException, TransactionException {
-        Caver caver = new Caver(Klay_HOST);
+        Caver caver = new Caver("");
         SingleKeyring keyring = KeyringFactory.createFromPrivateKey(fromPrivateKey);
         String fromAddress = keyring.toAccount().getAddress();
         //Add to caver wallet.
@@ -119,56 +121,12 @@ public class KlayController {
         return transactionReceipt;
     }
 
-    @RequestMapping("/klay/sendKlayTo")
-    public ResponseEntity<?> login(@RequestParam Map<String, Object> map) {
+    @RequestMapping("/klayScan/getAddressTxs")
+    public ResponseEntity<?> getAddressTxs(@RequestParam Map<String, Object> map) {
         if (map.get("address") == null || map.get("address").toString().length() == 0) {
             return new ResponseEntity(400, "address不能为空！");
         }
-        if (map.get("value") == null || map.get("value").toString().length() == 0) {
-            return new ResponseEntity(400, "value不能为空！");
-        }
-        TransactionReceipt.TransactionReceiptData result = null;
-        try {
-            result = sendingKLAY(SYSTEM_PRIVATE, map.get("address").toString()
-                    , BigInteger.valueOf(Long.parseLong(map.get("value").toString())));
-        } catch (Exception e) {
-            logger.error("发送sendingKLAY失败！", e);
-        }
-        return new ResponseEntity(result);
+        return new ResponseEntity(getAddressTxs(map.get("address").toString()));
     }
 
-    @RequestMapping("/klay/insertKlay")
-    public ResponseEntity insertKlay(@RequestParam Map<String, Object> map) {
-        if (map.get("address") == null || map.get("address").toString().length() == 0) {
-            return new ResponseEntity(400, "address不能为空！");
-        }
-        map.put("tableName", Klay_MANAGE);
-        int count = baseDao.insertIgnoreBase(map);
-        return new ResponseEntity(count);
-    }
-
-    @RequestMapping("/klay/updateKlay")
-    public ResponseEntity updateBaseByPrimaryKey(@RequestParam Map<String, Object> map) {
-        if (map.get("id") == null || map.get("id").toString().trim().length() == 0) {
-            return new ResponseEntity(400, "id不能为空！");
-        }
-        map.put("tableName", Klay_MANAGE);
-        int count = baseDao.updateBaseByPrimaryKey(map);
-        return new ResponseEntity(count);
-    }
-
-    @RequestMapping("/klay/getKlay")
-    public ResponseEntity getKlay(@RequestParam Map<String, Object> map) {
-        if (map.get("address") == null || map.get("address").toString().length() == 0) {
-            return new ResponseEntity(400, "address不能为空！");
-        }
-        map.put("tableName", Klay_MANAGE);
-        BaseModel baseModel = new BaseModel();
-        baseModel.setPageSize(1);
-        baseModel.setPageNo(1);
-        HashMap retmap = new HashMap();
-        List list = baseDao.selectBaseList(map, baseModel);
-        retmap.put("list", list);
-        return new ResponseEntity(retmap, 1, baseModel);
-    }
 }
---
+++ src/main/java/easyJava/entity/KlayTxsResult.java
@@ -0,0 +1,63 @@
+package easyJava.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+public class KlayTxsResult implements Serializable {
+
+    Boolean success;
+    Integer code;
+    Integer page;
+    Integer limit;
+    Integer total;
+    List<Map<String, Object>> result;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public List<Map<String, Object>> getResult() {
+        return result;
+    }
+
+    public void setResult(List<Map<String, Object>> result) {
+        this.result = result;
+    }
+}
