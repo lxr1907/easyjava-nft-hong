40a62e6b66e8ce1e0dc4db4586c4ea90d45f89e8
lxr <418982099@qq.com>
2022-03-31 21:55:59 +0800

    scan移动block

--- src/main/java/easyJava/controller/NFTScanController.java
+++ src/main/java/easyJava/controller/NFTScanController.java
@@ -56,48 +56,45 @@ public class NFTScanController {
         retList.forEach(map -> {
             map.put("tableName", ETH_LOG_TABLE);
             logger.info("------------map:" + JSON.toJSONString(map));
-            if (map.get("to").toString().equalsIgnoreCase(KlayController.SYSTEM_ADDRESS)
-                    && map.get("contract").toString().equalsIgnoreCase(ETH_USDT_CONTRACT_ADDRESS)) {
-                String amountStr = getDecimal18(map.get("value").toString());
-                logger.info("--------scanUSDTLogJob amountStr:" + amountStr
-                        + ",value" + map.get("value").toString() + "-------");
-                Map queryOrderMap = new HashMap();
-                queryOrderMap.put("tableName", KlayController.ORDER_TABLE);
-                queryOrderMap.put("send_value", amountStr);
-                queryOrderMap.put("status", 1);
-                BaseModel baseModel = new BaseModel();
-                baseModel.setPageSize(1);
-                baseModel.setPageNo(1);
-                List<Map> list = baseDao.selectBaseList(queryOrderMap, baseModel);
-                if (list.size() != 0) {
-                    //匹配到了订单金额完全相符的，认为是该用户的订单成功支付
-                    Map matchOrder = list.get(0);
-                    logger.info("-----------匹配到订单：" + JSON.toJSONString(matchOrder) + "----------");
-                    String user_id = matchOrder.get("user_id").toString();
-                    Map userQuery = new HashMap();
-                    userQuery.put("tableName", UserController.USER_TABLE);
-                    userQuery.put("id", Long.parseLong(user_id));
-                    Map user = baseDao.selectBaseByPrimaryKey(userQuery);
-                    String chr_address = user.get("chr_address").toString();
-                    logger.info("-----------匹配到订单user:" + JSON.toJSONString(user));
-                    long buy_amount = Long.parseLong(matchOrder.get("buy_amount").toString());
-                    long price = Long.parseLong(matchOrder.get("price").toString());
-                    BigInteger chrVal = BigInteger.valueOf(buy_amount * price);
-                    matchOrder.put("status", 2);
+            String amountStr = getDecimal18(map.get("value").toString());
+            logger.info("--------scanUSDTLogJob amountStr:" + amountStr
+                    + ",value" + map.get("value").toString() + "-------");
+            Map queryOrderMap = new HashMap();
+            queryOrderMap.put("tableName", KlayController.ORDER_TABLE);
+            queryOrderMap.put("send_value", amountStr);
+            queryOrderMap.put("status", 1);
+            BaseModel baseModel = new BaseModel();
+            baseModel.setPageSize(1);
+            baseModel.setPageNo(1);
+            List<Map> list = baseDao.selectBaseList(queryOrderMap, baseModel);
+            if (list.size() != 0) {
+                //匹配到了订单金额完全相符的，认为是该用户的订单成功支付
+                Map matchOrder = list.get(0);
+                logger.info("-----------匹配到订单：" + JSON.toJSONString(matchOrder) + "----------");
+                String user_id = matchOrder.get("user_id").toString();
+                Map userQuery = new HashMap();
+                userQuery.put("tableName", UserController.USER_TABLE);
+                userQuery.put("id", Long.parseLong(user_id));
+                Map user = baseDao.selectBaseByPrimaryKey(userQuery);
+                String chr_address = user.get("chr_address").toString();
+                logger.info("-----------匹配到订单user:" + JSON.toJSONString(user));
+                long buy_amount = Long.parseLong(matchOrder.get("buy_amount").toString());
+                long price = Long.parseLong(matchOrder.get("price").toString());
+                BigInteger chrVal = BigInteger.valueOf(buy_amount * price);
+                matchOrder.put("status", 2);
 
-                    matchOrder.put("tableName", KlayController.ORDER_TABLE);
+                matchOrder.put("tableName", KlayController.ORDER_TABLE);
+                baseDao.updateBaseByPrimaryKey(matchOrder);
+                try {
+                    logger.info("-----------sendingKLAY to user chr_address:------"
+                            + chr_address + ",val:" + chrVal);
+                    KlayController.sendingKLAY(KlayController.SYSTEM_PRIVATE, chr_address, chrVal);
+                    matchOrder.put("status", 3);
                     baseDao.updateBaseByPrimaryKey(matchOrder);
-                    try {
-                        logger.info("-----------sendingKLAY to user chr_address:------"
-                                + chr_address + ",val:" + chrVal);
-                        KlayController.sendingKLAY(KlayController.SYSTEM_PRIVATE, chr_address, chrVal);
-                        matchOrder.put("status", 3);
-                        baseDao.updateBaseByPrimaryKey(matchOrder);
-                        logger.info("-----------sendingKLAY to user chr_address:"
-                                + chr_address + ",val:" + chrVal + ",success-----");
-                    } catch (Exception e) {
-                        logger.error("sendingKLAY error", e);
-                    }
+                    logger.info("-----------sendingKLAY to user chr_address:"
+                            + chr_address + ",val:" + chrVal + ",success-----");
+                } catch (Exception e) {
+                    logger.error("sendingKLAY error", e);
                 }
             }
             baseDao.insertIgnoreBase(map);
