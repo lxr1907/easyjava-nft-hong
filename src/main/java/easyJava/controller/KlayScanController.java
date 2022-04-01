93267bae0838347d8d8da6bbd5279a458c141c3d
418982099@qq.com <418982099@qq.com>
2022-04-01 22:09:09 +0800

    获取地址的tokens chr余额

--- src/main/java/easyJava/controller/KlayScanController.java
+++ src/main/java/easyJava/controller/KlayScanController.java
@@ -11,14 +11,11 @@ import com.klaytn.caver.transaction.type.ValueTransfer;
 import com.klaytn.caver.wallet.keyring.KeyringFactory;
 import com.klaytn.caver.wallet.keyring.SingleKeyring;
 import easyJava.dao.master.BaseDao;
-import easyJava.entity.BaseModel;
 import easyJava.entity.KlayTxsResult;
 import easyJava.entity.ResponseEntity;
-import easyJava.etherScan.EventList;
 import easyJava.utils.HttpUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -29,9 +26,7 @@ import org.web3j.crypto.CipherException;
 import org.web3j.protocol.exceptions.TransactionException;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -46,8 +41,8 @@ public class KlayScanController {
 
     public static final String KLAY_TXS_TABLE = "klay_txs";
     public static final String KLAY_API_PRE = "https://api-baobab-v2.scope.klaytn.com/v2/accounts/";
+    public static final String KLAY_CHR_API_TAIL = "/ftBalances";
     public static final String TXS_API = "/txs";
-
     @Scheduled(cron = "*/50 * * * * ?")
     @RequestMapping("/scanKlayTxs")
     public ResponseEntity<?> scanKlayTxs() {
@@ -62,7 +57,18 @@ public class KlayScanController {
         });
         return new ResponseEntity();
     }
+    @RequestMapping("/klayScan/getAddressTokens")
+    public ResponseEntity<?> getAddressTokens(@RequestParam Map<String, Object> map) {
+        if (map.get("address") == null || map.get("address").toString().length() == 0) {
+            return new ResponseEntity(400, "address不能为空！");
+        }
+        return new ResponseEntity(getAddressTokens(map.get("address").toString()));
+    }
 
+    public static Object getAddressTokens(String address) {
+        String result = HttpUtil.get(KLAY_API_PRE + address + KLAY_CHR_API_TAIL);
+        return JSON.parse(result);
+    }
     /**
      * 获取privateKey对应的address
      *
