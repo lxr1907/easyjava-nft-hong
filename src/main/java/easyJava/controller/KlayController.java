090245c9898199858586f37532b97c6acbbb920e
lxr <418982099@qq.com>
2022-04-06 23:13:38 +0800

    合约地址变更为带提现功能合约

--- src/main/java/easyJava/controller/KlayController.java
+++ src/main/java/easyJava/controller/KlayController.java
@@ -51,7 +51,7 @@ public class KlayController {
     public static final String MY_KLAY_HOST = "http://43.132.248.207:8551";
 
     //合约币chr在klay链的地址
-    public static final String KLAY_CHR_ADDRESS = "0xc3aCA86099BCcf65EdC65E83fC7CCFae6bbe87dC";
+    public static final String KLAY_CHR_ADDRESS = "0xD3CFb75cE8Ed4Cbe10e7E343676a4788eC148d50";
     //usdt-erc20充值地址,ropsten测试链
     public static final String USDT_ADDRESS_ERC20_ROPSTEN = "0xC4f459a93169bbF3CF9Dc3c50D34502473703FB0";
     //usdt-erc20充值地址,rinkeby测试链
