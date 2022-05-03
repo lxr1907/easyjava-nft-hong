b10060ade10446d92bd926ce409a1fb10b0ec4d8
418982099@qq.com <418982099@qq.com>
2022-05-04 00:26:42 +0800

    scn接口,用chr兑换gamecoin，1:1

--- contract/chr.sol
+++ contract/chr.sol
@@ -5,40 +5,40 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CHRToken is ERC20,Ownable {
+contract CHRToken is ERC20, Ownable {
 
     //价格 1 klay = onePrice个 chr
-    uint256 public onePrice=1;
+    uint256 public onePrice = 1;
     constructor(uint256 initialSupply) ERC20("chr", "CHR") {
-        _mint(msg.sender,initialSupply*(10**uint256(decimals())));
+        _mint(msg.sender, initialSupply * (10 ** uint256(decimals())));
     }
 
-    receive () external payable
+    receive() external payable
     {
         uint256 amount = msg.value;
-        _mint(msg.sender, amount*onePrice);
+        _mint(msg.sender, amount * onePrice);
     }
     //提现chr兑换为klay
-    function withDraw(uint256 amount,address payable receiver)
+    function withDraw(uint256 amount, address payable receiver)
     payable
-    public onlyOwner{
-        require(balanceOf(receiver)>=amount);
-        _burn(receiver,amount);
-        receiver.transfer(amount/onePrice);
+    public onlyOwner {
+        require(balanceOf(receiver) >= amount);
+        _burn(receiver, amount);
+        receiver.transfer(amount / onePrice);
+    }
+    //todo 该方法还没有部署
+    function burn(uint256 amount, address receiver)
+    public onlyOwner {
+        require(balanceOf(receiver) >= amount);
+        _burn(receiver, amount);
     }
 
-    function burn(uint256 amount,address  receiver)
-    public onlyOwner{
-        require(balanceOf(receiver)>=amount);
-        _burn(receiver,amount);
-    }
-
-    function getBalance()public view
+    function getBalance() public view
     returns (uint256 balance){
         balance = address(this).balance;
     }
     //修改价格
-    function setPrice(uint256 price)public onlyOwner{
-        onePrice=price;
+    function setPrice(uint256 price) public onlyOwner {
+        onePrice = price;
     }
 }
