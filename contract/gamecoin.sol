db85f705b7da4c63fc2d02a4b56ca64513d3b8b1
418982099@qq.com <418982099@qq.com>
2022-05-18 23:14:04 +0800

    addBuyOrder addSaleOrder

--- contract/gamecoin.sol
+++ contract/gamecoin.sol
@@ -122,13 +122,14 @@ contract GameCoin is ERC20, Ownable {
     {
         uint256 amount = msg.value;
         require(msg.value>0 && myprice>0);
+        OrderEntity memory newOrder=OrderEntity({
+        amount:amount.mul(myprice),
+        price:myprice,
+        time:block.timestamp,
+        sender:msg.sender
+        });
         if(buyOrdersArray.length==0){
-            buyOrdersArray.push(OrderEntity({
-            amount:amount,
-            price:myprice,
-            time:block.timestamp,
-            sender:msg.sender
-            }));
+            buyOrdersArray.push(newOrder);
             return;
         }
         bool startMove = false;
@@ -140,27 +141,18 @@ contract GameCoin is ERC20, Ownable {
             }else{
                 buyHighOrder = buyOrdersArray[i];
                 if( myprice < buyHighOrder.price){
-                    buyOrdersArray[i] =  OrderEntity({
-                    amount:amount,
-                    price:myprice,
-                    time:block.timestamp,
-                    sender:msg.sender
-                    });
+                    buyOrdersArray[i] =  newOrder;
                     startMove = true;
                 }
             }
         }
 
         if(!startMove){
-            buyOrdersArray.push(OrderEntity({
-            amount:amount,
-            price:myprice,
-            time:block.timestamp,
-            sender:msg.sender
-            }));
+            buyOrdersArray.push(newOrder);
         }else{
             buyOrdersArray.push(buyHighOrder);
         }
+        matchBuyOrder();
     }
 
     //取消挂单，购买gamecoin
@@ -168,6 +160,7 @@ contract GameCoin is ERC20, Ownable {
     {
         bool startMove = false;
         uint256 amount=0;
+        uint256 price=1;
         for (uint i = 0; i < buyOrdersArray.length; i++) {
             if(startMove){
                 if(i+1< buyOrdersArray.length){
@@ -177,6 +170,7 @@ contract GameCoin is ERC20, Ownable {
                 if( time == buyOrdersArray[i].time
                     && buyOrdersArray[i].sender == msg.sender){
                     amount=buyOrdersArray[i].amount;
+                    price=buyOrdersArray[i].price;
                     if(i+1< saleOrdersArray.length){
                         buyOrdersArray[i] = buyOrdersArray[i+1];
                     }
@@ -186,7 +180,7 @@ contract GameCoin is ERC20, Ownable {
         }
         delete buyOrdersArray[buyOrdersArray.length-1];
         address payable receiver= payable(msg.sender);
-        receiver.transfer(amount);
+        receiver.transfer(amount.div(price));
     }
 
 
@@ -202,20 +196,20 @@ contract GameCoin is ERC20, Ownable {
                 break;
             }
             //当前订单对应的chr总数
-            uint256 gamecoinAmount=buyOrdersArray[i].amount.mul(buyOrdersArray[i].price);
+            uint256 gamecoinAmount=buyOrdersArray[i].amount;
             if(gamecoinAmount>gamecoinPayed){
                 //当前订单总数充足，则部分成交
                 uint256 gamecoinAmountLeft=gamecoinAmount.sub(gamecoinPayed);
-                uint256 chrLeft=gamecoinAmountLeft.div(buyOrdersArray[i].price);
-                uint256 chrGet=buyOrdersArray[i].amount.sub(chrLeft);
-                buyOrdersArray[i].amount=chrLeft;
+                uint256 chrGet=gamecoinPayed.div(buyOrdersArray[i].price);
+                buyOrdersArray[i].amount=gamecoinAmountLeft;
+                gamecoinPayed=0;
                 //加chr
                 payable(msg.sender).transfer(chrGet);
                 break;
             }else{
                 //当前订单总数不足，则消耗完该订单继续循环
                 gamecoinPayed = gamecoinPayed.sub(gamecoinAmount);
-                uint256 chrGet=buyOrdersArray[i].amount;
+                uint256 chrGet=gamecoinAmount.div(buyOrdersArray[i].price);
                 //删除消耗掉的这个订单
                 delete buyOrdersArray[i];
                 i=i-1;
@@ -234,7 +228,7 @@ contract GameCoin is ERC20, Ownable {
     //撮合购买gamecoin挂单
     function matchBuyOrder()  private
     {
-        uint256 chrPayed = buyOrdersArray[0].amount;
+        uint256 gamecoinWant = buyOrdersArray[0].amount;
         uint256 price = buyOrdersArray[0].price;
         for (uint i = 0; i < saleOrdersArray.length; i++) {
             if(price>saleOrdersArray[i].price){
@@ -242,19 +236,18 @@ contract GameCoin is ERC20, Ownable {
                 break;
             }
             //当前订单对应的chr总数
-            uint256 chrAmount=saleOrdersArray[i].amount.div(saleOrdersArray[i].price);
-            if(chrAmount>chrPayed){
+            uint256 gamecoinAmount=saleOrdersArray[i].amount;
+            if(gamecoinAmount>gamecoinWant){
                 //当前订单总数充足，则部分成交
-                uint256 chrAmountLeft=chrAmount.sub(chrPayed);
-                uint256 gamecoinLeft=chrAmountLeft.mul(saleOrdersArray[i].price);
-                uint256 gamecoinGet=saleOrdersArray[i].amount.sub(gamecoinLeft);
+                uint256 gamecoinLeft=gamecoinAmount.sub(gamecoinWant);
                 saleOrdersArray[i].amount=gamecoinLeft;
+                gamecoinWant=0;
                 //加gamecoin
-                _mint(msg.sender,gamecoinGet);
+                _mint(msg.sender,gamecoinWant);
                 break;
             }else{
                 //当前订单总数不足，则消耗完该订单继续循环
-                chrPayed = chrPayed.sub(chrAmount);
+                gamecoinWant = gamecoinWant.sub(gamecoinAmount);
                 uint256 gamecoinGet=saleOrdersArray[i].amount;
                 //删除消耗掉的这个订单
                 delete saleOrdersArray[i];
@@ -263,9 +256,9 @@ contract GameCoin is ERC20, Ownable {
                 _mint(msg.sender,gamecoinGet);
             }
         }
-        if(chrPayed!=0){
+        if(gamecoinWant!=0){
             //剩余的金额不为0，则部分未成交
-            buyOrdersArray[0].amount=chrPayed;
+            buyOrdersArray[0].amount=gamecoinWant;
         }else{
             //剩余的金额为0，则删除
             delete buyOrdersArray[0];
@@ -282,20 +275,19 @@ contract GameCoin is ERC20, Ownable {
         require(balanceOf(msg.sender) >= gamecoinPayed);
         for (uint i = 0; i < buyOrdersArray.length; i++) {
             //当前订单对应的chr总数
-            uint256 gamecoinAmount=buyOrdersArray[i].amount.mul(buyOrdersArray[i].price);
+            uint256 gamecoinAmount=buyOrdersArray[i].amount;
             if(gamecoinAmount>gamecoinPayed){
                 //当前订单总数充足，则部分成交
                 uint256 gamecoinAmountLeft=gamecoinAmount.sub(gamecoinPayed);
-                uint256 chrLeft=gamecoinAmountLeft.div(buyOrdersArray[i].price);
-                uint256 chrGet=buyOrdersArray[i].amount.sub(chrLeft);
-                buyOrdersArray[i].amount=chrLeft;
+                uint256 chrGet=gamecoinAmount.div(buyOrdersArray[i].price);
+                buyOrdersArray[i].amount=gamecoinAmountLeft;
                 //加chr
                 payable(msg.sender).transfer(chrGet);
                 break;
             }else{
                 //当前订单总数不足，则消耗完该订单继续循环
                 gamecoinPayed = gamecoinPayed.sub(gamecoinAmount);
-                uint256 chrGet=buyOrdersArray[i].amount;
+                uint256 chrGet=buyOrdersArray[i].amount.div(buyOrdersArray[i].price);
                 //删除消耗掉的这个订单
                 delete buyOrdersArray[i];
                 i=i-1;
