64c742b789737cf9e0613294dc91d2daa4b2dfb9
418982099@qq.com <418982099@qq.com>
2022-07-07 23:43:21 +0800

    BuyGameItemThread

--- src/main/java/easyJava/controller/SCNGameCoinController.java
+++ src/main/java/easyJava/controller/SCNGameCoinController.java
@@ -781,7 +781,7 @@ public class SCNGameCoinController {
                 String url = "http://52.77.31.208/api/index/payback?no=" + no
                         + "&code=" + code
                         + "&time=" + time
-                        + "&info=" + ret.getTxError()
+                        + "&info=" + ret.getStatus()
                         + "&sign=" + sign;
                 String result = HttpUtil.httpGet(url);
                 logger.info("BuyGameItemThread payback result:" + result);
