addc66466594f40fb3b3189cfb9fa9d474e1a19f
418982099@qq.com <418982099@qq.com>
2022-07-10 01:05:10 +0800

    matchOrders

--- src/main/java/easyJava/controller/ScheduledController.java
+++ src/main/java/easyJava/controller/ScheduledController.java
@@ -27,14 +27,14 @@ public class ScheduledController {
     //开启EnableScheduling注解，设置定时任务
     @Scheduled(cron = "0/10 * * * * ?")
     public void matchOrders() {
-        executor.execute(new MatchBuyOrders());
-        executor.execute(new MatchSaleOrders());
+        executor.execute(new MatchOrders());
     }
 
-    class MatchBuyOrders extends Thread {
+    class MatchOrders extends Thread {
         @Override
         public void run() {
             long begin = new Date().getTime();
+
             try {
                 SCNGameCoinController.matchBuyOrder();
             } catch (Exception e) {
@@ -42,20 +42,13 @@ public class ScheduledController {
             }
             long step1End = new Date().getTime();
             logger.info("matchBuyOrder end, time:" + (step1End - begin));
-        }
-    }
-
-    class MatchSaleOrders extends Thread {
-        @Override
-        public void run() {
-            long begin = new Date().getTime();
             try {
                 SCNGameCoinController.matchSaleOrder();
             } catch (Exception e) {
                 logger.error("matchSaleOrder thread error", e);
             }
-            long end = new Date().getTime();
-            logger.info("matchSaleOrder end, time:" + (begin - end));
+            long step2End = new Date().getTime();
+            logger.info("matchSaleOrder end, time:" + (step2End - step1End));
         }
     }
 }
