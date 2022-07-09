97eafcfc2f0f664387ec82386226b124745c4858
418982099@qq.com <418982099@qq.com>
2022-07-09 23:19:27 +0800

    增加matchOrders定时器

--- src/main/java/easyJava/controller/SCNGameCoinController.java
+++ src/main/java/easyJava/controller/SCNGameCoinController.java
@@ -24,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
--- src/main/java/easyJava/controller/ScheduledController.java
+++ src/main/java/easyJava/controller/ScheduledController.java
@@ -2,18 +2,42 @@ package easyJava.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
-//@EnableScheduling
+@EnableScheduling
 public class ScheduledController {
-	private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledController.class);
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledController.class);
 
-	//开启EnableScheduling注解，设置定时任务
-	@Scheduled(cron = "0 */1 * * * ?")
-	public void createTask() {
+    //开启EnableScheduling注解，设置定时任务
+    @Scheduled(cron = "* * * * * ?")
+    public void matchOrders() {
+        new matchOrders().start();
+    }
 
-	}
+    class matchOrders extends Thread {
+        @Override
+        public void run() {
+            long begin = new Date().getTime();
 
+            try {
+                SCNGameCoinController.matchBuyOrder();
+            } catch (Exception e) {
+                logger.error("matchBuyOrder thread error", e);
+            }
+            long step1End = new Date().getTime();
+            logger.info("matchBuyOrder end, time:" + (step1End - begin));
+            try {
+                SCNGameCoinController.matchSaleOrder();
+            } catch (Exception e) {
+                logger.error("matchSaleOrder thread error", e);
+            }
+            long step2End = new Date().getTime();
+            logger.info("matchSaleOrder end, time:" + (step2End - step1End));
+        }
+    }
 }
